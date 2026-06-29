b7ee1660e1182916b69409bed918d4146b2284d4
Sohan Patnaik <sohanpatnaik9@gmail.com>
2026-06-29 17:29:22 +0530

    corrected code

--- routes/agent.py
+++ routes/agent.py
@@ -1,15 +1,7 @@
 from fastapi import APIRouter, HTTPException
-<<<<<<< HEAD
 from controllers.agent_controller import AgentController
 from pydantic import BaseModel
 import traceback
-=======
-from fastapi.responses import StreamingResponse
-from backend.controllers.agent_controller import AgentController
-from pydantic import BaseModel
-import traceback
-
->>>>>>> 91acf44 (added chat stream api)
 router = APIRouter()
 
 
@@ -20,7 +12,6 @@ class ChatRequest(BaseModel):
 
 @router.post("/chat")
 def chat(request: ChatRequest):
-<<<<<<< HEAD
 
     try:
 
@@ -28,37 +19,12 @@ def chat(request: ChatRequest):
 
         result = controller.run(request.msg)
 
-=======
-    try:
-        controller = AgentController(request.bot_id)
-        result = controller.run(request.msg)
->>>>>>> 91acf44 (added chat stream api)
         return {
             "bot_id": request.bot_id,
             "reply": result.get("answer"),
             "contexts": result.get("contexts"),
             "latency": result.get("latency")
         }
-<<<<<<< HEAD
-
-=======
-    except Exception as e:
-        traceback.print_exc()
-        raise HTTPException(
-            status_code=500,
-            detail=str(e)
-        )
-
-
-@router.post("/stream")
-def stream_chat(request: ChatRequest):
-    try:
-        controller = AgentController(request.bot_id)
-        return StreamingResponse(
-            controller.stream_run(request.msg),
-            media_type="text/plain"
-        )
->>>>>>> 91acf44 (added chat stream api)
     except Exception as e:
         traceback.print_exc()
         raise HTTPException(
